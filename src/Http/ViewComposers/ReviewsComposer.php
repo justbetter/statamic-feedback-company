2a8c2f2e7346532e271fde7d780ef8567051a83d
Jade Geels <jade@justbetter.nl>
2023-05-08 12:55:46 +0200

    Don't just assume rapidez/statamic is installed

--- src/Http/ViewComposers/ReviewsComposer.php
+++ src/Http/ViewComposers/ReviewsComposer.php
@@ -5,6 +5,7 @@ namespace JustBetter\StatamicFeedbackCompany\Http\ViewComposers;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\View\View;
 use Statamic\Facades\Entry;
+use Statamic\Facades\GlobalSet;
 
 class ReviewsComposer
 {
@@ -12,9 +13,9 @@ class ReviewsComposer
     {
         $reviews = Cache::remember('feedback-company-data', now()->addHour(), function () use ($view) {
             $count = Entry::query()->where('collection', 'reviews')->count();
-            $score = $view->globals->reviews->average_score ?? null;
-            $percentage = $view->globals->reviews->recommendation_percentage ?? null;
-
+            $reviews = $this->reviewsGlobal($view);
+            $score = $reviews->average_score ?? null;
+            $percentage = $reviews->recommendation_percentage ?? null;
             $counts = [];
             for($i = 1; $i <= 10; $i++) {
                 $counts[$i] = Entry::query()->where('collection', 'reviews')->where('total_score', $i)->count();
@@ -24,5 +25,18 @@ class ReviewsComposer
         });
 
         $view->with('reviews', $reviews);
+    }
+
+    protected function reviewsGlobal(View $view)
+    {
+        if($view->globals->reviews ?? null) {
+            return $view->globals->reviews;
+        } else {
+            $set = GlobalSet::findByHandle('reviews');
+            if (! $set || ! $set->localizations()['default']) {
+                return [];
+            }
+            return $set->localizations()['default'];
+        }
     }
 }
